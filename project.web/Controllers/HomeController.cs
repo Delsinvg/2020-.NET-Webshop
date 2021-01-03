7000677d30509f4a0c2c571f8312cf7019647b15
Delsin Van Grembergen <delsin.vangrembergen@student.odisee.be>
2021-01-03 15:18:21 +0100

    [Web] Multilingual

--- project.web/Controllers/HomeController.cs
+++ project.web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using project.web.Models;
+using System;
 using System.Diagnostics;
 
 namespace project.web.Controllers
@@ -12,6 +15,18 @@ namespace project.web.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
         }
 
         public IActionResult Index()
