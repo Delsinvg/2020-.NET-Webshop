046f97e16c754b4165aa09a924415ac10e090d04
Delsin Van Grembergen <delsin.vangrembergen@student.odisee.be>
2021-01-10 20:24:30 +0100

    [Web] Some changes

--- project.models/Orders/OrderProductModel.cs
+++ project.models/Orders/OrderProductModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace project.models.Orders
 {
     public class OrderProductModel
     {
+        [Display(Name = "Product id")]
         public Guid ProductId { get; set; }
+        [Display(Name = "Naam")]
         public String Name { get; set; }
+        [Display(Name = "Hoeveelheid")]
         public int Quantity { get; set; }
+        [Display(Name = "Prijs")]
         public Decimal Price { get; set; }
     }
 }
